50c1732abb1b485d5b839a6d0ecc4ddcb960bf46
iamamawhoiami <117635714+iamamawhoiami@users.noreply.github.com>
2022-11-11 14:48:23 +0600

    add

---
+++ js/main.js
@@ -0,0 +1,43 @@
+(function ($) {
+  "use strict";
+
+  var fullHeight = function () {
+    $(".js-fullheight").css("height", $(window).height());
+    $(window).resize(function () {
+      $(".js-fullheight").css("height", $(window).height());
+    });
+  };
+  fullHeight();
+
+  var carousel = function () {
+    $(".featured-carousel").owlCarousel({
+      arrows: false,
+      loop: true,
+      autoplay: false,
+      margin: 30,
+      animateOut: "fadeOut",
+      animateIn: "fadeIn",
+      nav: true,
+      dots: true,
+      autoplayHoverPause: false,
+      items: 1,
+      mouseDrag: true,
+
+      responsive: {
+        0: {
+          items: 1,
+        },
+        600: {
+          items: 2,
+        },
+        1000: {
+          items: 3,
+        },
+        1297: {
+          items: 4,
+        },
+      },
+    });
+  };
+  carousel();
+})(jQuery);
